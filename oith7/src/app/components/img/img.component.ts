3e7c3e1bbba641f7409759c23c08543c3422284c
Jared Parkinson <jared@parkinson.im>
2019-10-30 05:27:01 -0600

    img supprt

--- oith7/src/app/components/img/img.component.ts
+++ oith7/src/app/components/img/img.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormatGroup } from "../../../../../oith-lib/src/processors/Chapter";
 
 @Component({
-  selector: 'app-img',
-  templateUrl: './img.component.html',
-  styleUrls: ['./img.component.scss']
+  selector: "app-img",
+  templateUrl: "./img.component.html",
+  styleUrls: ["./img.component.scss"]
 })
 export class ImgComponent implements OnInit {
-
-  constructor() { }
+  @Input() public img: FormatGroup;
+  public src: string;
+  public alt: string;
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.img);
+    this.src = `assets/images/${
+      this.img.attrs ? this.img.attrs["src"] : ""
+    }.jpg`;
+    this.alt = this.img.attrs ? this.img.attrs["alt"] : "";
   }
-
 }
