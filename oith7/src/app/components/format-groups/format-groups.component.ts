592e9f52677932e66aa2b850ee3152325d7d5bf3
Jared Parkinson <jared@parkinson.im>
2019-10-31 01:42:47 -0600

    content

--- oith7/src/app/components/format-groups/format-groups.component.ts
+++ oith7/src/app/components/format-groups/format-groups.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormatGroup } from '../../../../../oith-lib/src/processors/Chapter';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormatGroup } from "../../../../../oith-lib/src/processors/Chapter";
 
 @Component({
-  selector: 'app-format-groups',
-  templateUrl: './format-groups.component.html',
-  styleUrls: ['./format-groups.component.scss']
+  selector: "app-format-groups",
+  templateUrl: "./format-groups.component.html",
+  styleUrls: ["./format-groups.component.scss"]
 })
 export class FormatGroupsComponent implements OnInit {
-  @Input() public formatGroups?: FormatGroup[];
+  @Input() public formatGroups: FormatGroup;
   constructor() {}
 
   ngOnInit() {}
